1bf874fecf4f75757a8c1f513e12f23960e15f56
Kennydamenni <736659@student.dusd.net>
2025-11-16 14:35:02 -0800

    changed the code
    made left and right keys move the charecter

--- P6WeimerKenneth2DBegginerAdventure/Assets/scripts/PlayerController.cs
+++ P6WeimerKenneth2DBegginerAdventure/Assets/scripts/PlayerController.cs
@@ -15,6 +15,7 @@ public class PlayerController : MonoBehaviour
     //Variables related to the health system
     public int maxHealth = 5;
     int currentHealth;
+    private Vector2 position;
 
 
     // Start is called before the first frame update
@@ -28,21 +29,39 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        float horizontal = 0.0f;
+        if (Keyboard.current.leftArrowKey.isPressed)
+        {
+            horizontal = -1.0f;
+        }
+        else if (Keyboard.current.rightArrowKey.isPressed)
+        {
+            horizontal = 1.0f;
+        }
+
+        Debug.Log(horizontal);
+        
         move = MoveAction.ReadValue<Vector2>();
         Debug.Log(move);
+        Vector2 position = transform.position;
+        position.x = position.x + 0.1f * horizontal;
+        transform.position = position;
+
     }
 
 
     // FixedUpdate has the same call rate as the physics system
     private void FixedUpdate()
     {
-       Vector2 postion = (Vector2)rigidbody2d.postion + move hyyy
-
+        Vector2 postion = (Vector2)rigidbody2d.position + move * speed * Time.deltaTime;
+        rigidbody2d.MovePosition(position);
     }
 
 
-
-
-
+    void ChangeHealth (int amouunt)
+    {
+        currentHealth = Mathf.Clamp(currentHealth, 0, maxHealth);
+        Debug.Log(currentHealth + "/" + maxHealth);
+    }
 }
 
