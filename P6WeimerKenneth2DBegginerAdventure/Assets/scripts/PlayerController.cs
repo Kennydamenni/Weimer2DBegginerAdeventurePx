cebf56be650f6fa3fe009f7bc12375f32622b7ff
Kennydamenni <736659@student.dusd.net>
2025-11-16 10:56:14 -0800

    made changes to code
    that it

--- P6WeimerKenneth2DBegginerAdventure/Assets/scripts/PlayerController.cs
+++ P6WeimerKenneth2DBegginerAdventure/Assets/scripts/PlayerController.cs
@@ -1,21 +1,48 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    //variables related to player charecter movement
+    public InputAction MoveAction;
+    Rigidbody2D rigidbody2d;
+    Vector2 move;
+    public float speed = 3.0f;
+
+
+    //Variables related to the health system
+    public int maxHealth = 5;
+    int currentHealth;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        MoveAction.Enable();
+        rigidbody2d = GetComponent<Rigidbody2D>();
+        currentHealth = maxHealth;
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector2 position = transform.position;
-        position.x = position.x + 0.001f;
-        transform.position = position;
+        move = MoveAction.ReadValue<Vector2>();
+        Debug.Log(move);
+    }
+
+
+    // FixedUpdate has the same call rate as the physics system
+    private void FixedUpdate()
+    {
+       Vector2 postion = (Vector2)rigidbody2d.postion + move hyyy
 
     }
+
+
+
+
+
 }
+
