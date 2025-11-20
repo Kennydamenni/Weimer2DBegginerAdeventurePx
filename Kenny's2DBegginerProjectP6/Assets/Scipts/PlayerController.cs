35f2797a7050511d9c6f7736367394fb57ea0bbb
Kennydamenni <736659@student.dusd.net>
2025-11-20 07:37:24 -0800

    added tiles but then they stopped working but the project works
    the code dosent work

---
+++ Kenny's2DBegginerProjectP6/Assets/Scipts/PlayerController.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.UIElements;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public InputAction LeftAction;
+   
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    
+    
+    }
