05b92aa600710ec2062e6335db5e4b93c4c4854a
Kennydamenni <736659@student.dusd.net>
2025-12-04 19:45:00 -0800

    fixed code
    added health code for health item

--- Kenny's2DBegginerProjectP6/Assets/Scipts/PlayerController.cs
+++ Kenny's2DBegginerProjectP6/Assets/Scipts/PlayerController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,10 +7,16 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    // Variables related to player charecter movement 
+    // Variables related to player character movement 
     public InputAction MoveAction;
     Rigidbody2D rigidbody2d;
     Vector2 move;
+    public float speed = 3.0f;
+
+
+    // variables related to the health system 
+    public int maxHealth = 5;
+    int currentHealth = 1;
 
 
     // Start is called before the first frame update
@@ -17,17 +24,29 @@ public class PlayerController : MonoBehaviour
     {
         MoveAction.Enable();
         rigidbody2d = GetComponent<Rigidbody2D>();
+        // currentHealth = maxHealth;
     }
 
     // Update is called once per frame
     void Update()
     {
-        move = MoveAction.ReadValue<Vector2>();
-        Debug.Log(move);
+       move = MoveAction.ReadValue<Vector2>();
     }
+    // FixedUpdate has the same call rate as the rythem system
     void FixedUpdate()
     {
-        Vector2 position = (Vector2)rigidbody2d.position + move * 3.0f * Time.deltaTime;
+        Vector2 position = (Vector2)rigidbody2d.position + move * speed * Time.deltaTime;
         rigidbody2d.MovePosition(position);
     }
+
+
+     public void ChangeHealth (int amount)
+    {
+      currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
+      Debug.Log(currentHealth + "/" + maxHealth);
+    }
+
+
 }
+
+
